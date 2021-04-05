bc1f5a1bf9a4164110aaec3f226d80f210cb31fe
Saidur Rahman Setu <srsetu@gmail.com>
2021-04-05 23:33:49 +0600

    Till While Loops

---
+++ 6.1_what_is_loops.js
@@ -0,0 +1,7 @@
+let arr = [];
+
+for (i = 5; i <= 5; i = 5) {
+    arr.push(i);
+}
+
+console.log(arr);
---
+++ 6.2_for.js
@@ -0,0 +1,22 @@
+// initialization
+// condition
+// final statement
+// let i = 0;
+// for (; i < 5; ) {
+//     // console.log(i);
+//     // i++;
+// }
+
+// console.log(i);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+for (let i = 1; i <= 1000; i++) {
+    if (i % 3 === 0 && i % 25 === 0) {
+        console.log(i);
+    }
+}
