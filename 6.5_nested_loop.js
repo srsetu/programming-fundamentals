46ae7c8a05776132fe55156f7d25d32420ac2169
Saidur Rahman Setu <srsetu@gmail.com>
2021-04-19 20:25:41 +0600

    6.6 For Of Added

---
+++ 6.5_nested_loop.js
@@ -0,0 +1,14 @@
+let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9, 10],
+];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
+    }
+}
+
+// i => row
+// j => column
---
+++ 6.6_for_of.js
@@ -0,0 +1,11 @@
+let arr = [1, 3, 4, 5];
+
+for (let el of arr) {
+    console.log(el);
+}
+
+let name = 'Saidur Rahman';
+
+for (let str of name) {
+    console.log(str);
+}
